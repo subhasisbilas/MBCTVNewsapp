fd67827f9a6e6dbe33dfa0e9ab530592aa539f3d
Jayesh Karathiya <jayeshsagar@hotmail.com>
2021-07-02 20:51:23 +0100

    Start running application

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  FlatList,
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+} from 'react-native';
+
+import { ArticleRow } from './app/components/ArticleRow';
+import { client } from './app/graphql/client';
+import { TopHeadlines } from './app/graphql/queries';
 
 export default function App() {
+  const [articles, setArticles] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const requestApollo = () => {
+    client
+      .query({ query: TopHeadlines })
+      .then(response => {
+        console.log(response);
+        setArticles(response.data.headlines.articles);
+        setLoading(response.loading);
+      })
+      .catch(err => {
+        console.log('err', err);
+      });
+  };
+
+  useEffect(() => {
+    requestApollo();
+  })
+
+  const renderFooter = () => {
+    if (loading) {
+      return <ActivityIndicator size="large" />;
+    }
+
+    return null;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <FlatList
+        data={articles}
+        ListHeaderComponent={
+          <Text style={styles.headerText}>Top Headlines</Text>
+        }
+        renderItem={({ item, index }) => (
+          <ArticleRow index={index} {...item} />
+        )}
+        keyExtractor={item => `${item.publishedAt}-${item.title}`}
+        ListFooterComponent={renderFooter()}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  headerText: {
+    color: '#ff8d01',
+    fontWeight: '900',
+    fontSize: 40,
+    paddingHorizontal: 10,
+    marginBottom: 30,
+    marginTop: 10,
   },
 });
